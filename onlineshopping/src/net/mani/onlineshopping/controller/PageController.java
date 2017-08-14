2bc96f7a18e79505d6262144b6bd88aced1be71a
Lokendra Mani <lokendramani@gmail.com>
2017-08-14 13:14:51 +0530

    Completed the interface theme and other issues.

--- onlineshopping/src/net/mani/onlineshopping/controller/PageController.java
+++ onlineshopping/src/net/mani/onlineshopping/controller/PageController.java
@@ -10,7 +10,26 @@ public class PageController {
 	@RequestMapping(value={"/","/home","/index"})
 	public ModelAndView index(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring MVC");
+		mv.addObject("title", "Welcome Aboard For Online Shopping");
+		mv.addObject("userClickHome", true);
+		return mv;
+		
+	}
+	
+	@RequestMapping(value="/contact")
+	public ModelAndView contact(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "Contact");
+		mv.addObject("userClickContact", true);
+		return mv;
+		
+	}
+	
+	@RequestMapping(value="/about")
+	public ModelAndView about(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
 		return mv;
 		
 	}
